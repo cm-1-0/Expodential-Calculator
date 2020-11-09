0096648b52bb512fa41e3151758d6f9fe18c03e7
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2020-11-09 15:43:57 -0600

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,7 @@
+# Expo Calc section
+base_num = input("Enter a base number: ")
+pow_num = input("Enter a power: ")
+base_num2 = float(base_num)
+pow_num2 = float(pow_num)
+expo_calc = base_num2 ** pow_num2
+print(expo_calc)
